39e9c57f660718ff3f3d043f12e83015a1df7433
Rivergoat <pelle.haenel@outlook.de>
2017-12-25 13:33:23 +0100

    added noise fixing class

---
+++ FixNoise/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Drawing;
+using System.Math;
+
+namespace FixNoisue
+{
+    class Program
+    {
+        // empty space
+    }
+}
---
+++ FixNoise/Tools.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Drawing;
+using System.Math;
+using Module.Planar.Transformation;
+
+namespace FixNoise
+{
+    public static class Tools
+    {
+	
+    }
+}
